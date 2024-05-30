7d82f320a7ea34bed8d480549d70d9ec726ba25b
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-29 22:01:16 -0400

    First response sent

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
 import socket
 
+HOST = "localhost"
+PORT = 4221
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    conn, addr = server_socket.accept() # wait for client
+    with conn:
+        print(f"Connected by {addr}")
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        conn.close()
 
 if __name__ == "__main__":
     main()
